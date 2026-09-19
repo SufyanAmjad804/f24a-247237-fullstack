affc3888862804b1e33f73b81ef523a1cbb3063b
Your Name <you@example.com>
2026-09-19 23:23:33 +0500

    Task 15 — Final script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let label = "Read";
 console.log(label);
 
-document.querySelector("#status");
-
 document.querySelector("button").addEventListener("click", () => {
     document.querySelector("#status").textContent = label;
 });
